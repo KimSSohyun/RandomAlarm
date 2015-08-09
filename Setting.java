727c6d9b38276455046d032383ce49e56e212640
KimSSohyun <KimSSohyun@users.noreply.github.com>
2015-08-10 00:53:36 +0900

    Update Setting.java

--- Setting.java
+++ Setting.java
@@ -1,17 +1,27 @@
 package com.sohyun.gradproj.randomalarm;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ToggleButton;
 
+import java.io.File;
+import java.util.Calendar;
+
 /**
  * Created by Keiden on 2015-08-10.
  */
 public class Setting extends Activity{
+    private static final String BASE_PATH = Environment.getExternalStorageDirectory() + "";
+    private static final String NORMAL_PATH = BASE_PATH + "/media/MUSIC";
+    private AlarmManager _am;
+
     private Button selMusic, OK;
     private ToggleButton _toggleSun, _toggleMon, _toggleTue, _toggleWed, _toggleThu, _toggleFri, _toggleSat;
 
@@ -43,10 +53,48 @@ public class Setting extends Activity{
             @Override
             public void onClick(View v) {
                 Log.i("onClick", "CallsetActivity");
+                onRegist(v);
                 Intent intentsetActivity =
                         new Intent(Setting.this, MainActivity.class);
                 startActivity(intentsetActivity);
             }
         });
     }
+
+        public void onRegist(View v)
+    {
+        Log.i("Setting:onRegist", "|" + "========= regist" + "|");
+
+        File file = new File(NORMAL_PATH + "/drop_1235.m4a");
+        Log.i("MainActivity:onRegist", "| file exists? : " + file.exists() + "|" + file.hashCode());
+
+        boolean[] week = { false, _toggleSun.isChecked(), _toggleMon.isChecked(), _toggleTue.isChecked(), _toggleWed.isChecked(),
+                _toggleThu.isChecked(), _toggleFri.isChecked(), _toggleSat.isChecked() }; // sunday=1 이라서 0의 자리에는 아무 값이나 넣었음
+
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        intent.putExtra("file", file.toString());
+        intent.putExtra("weekday", week);
+        PendingIntent pIntent = PendingIntent.getBroadcast(this, file.hashCode(), intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.SECOND, cal.get(Calendar.SECOND) + 10); // 10초 뒤
+
+        long oneday = 24 * 60 * 60 * 1000;// 24시간
+
+        // 10초 뒤에 시작해서 매일 같은 시간에 반복하기
+        _am.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), oneday, pIntent);
+    }
+
+//    public void onUnregist(View v)
+//    {
+//        Log.i("MainActivity:onUnregist", "|" + "========= unregist" + "|");
+//
+//        File file = new File(NORMAL_PATH + "/drop_1235.m4a");
+//        Log.i("MainActivity:onRegist", "| file exists? : " + file.exists() + "|" + file.hashCode());
+//
+//        Intent intent = new Intent(this, AlarmReceiver.class);
+//        PendingIntent pIntent = PendingIntent.getBroadcast(this, file.hashCode(), intent, 0);
+//
+//        _am.cancel(pIntent);
+//    }
 }
